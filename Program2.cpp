87bfd5d698e3840531f5e9030229b4f4c2249a46
DELGADO242 <jdelgado48@saddleback.edu>
2024-09-04 03:24:20 +0000

    SEP 3 class. INT, COUT, CIN, TIMES TABLES.

--- Program2.cpp
+++ Program2.cpp
@@ -1,4 +1,14 @@
 #include<iostream>
 using namespace std; 
+
 int main() {
-    
+    int x;  // we ask c++ to get a memory location from RAM and name it x
+            // and we are going to store an int number (whole number) in x 
+    x=10;   // here we ask c++ tos tore value of 10 in memory location named x
+    int y;  // we get another memory location and name it y
+            // we dont stire a value in that memory location
+            // it will keep whatever from the time in the past that had been 
+            // stored in that memory location -- lets call it a garbage value
+  cout << "x; " << x << endl;
+  cout << "y; " << y << endl;   
+}
---
+++ p3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main () { 
+    int x, y, z;
+    z = x + y;
+     cin >> x; 
+     cin >> y; 
+     // cin >> x >> y;
+    z = x + y;
+    cout << z << endl;
+}
